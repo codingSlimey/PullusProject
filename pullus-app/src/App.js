5b66bcdac26edda300649212661491e1923bd9e8
codingSlimey <superslimeysl@gmail.com>
2022-10-27 16:32:05 +0300

    Created Reset Password page

--- pullus-app/src/App.js
+++ pullus-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css'
 import Home from './components/HOME/Home'
 import Login from './components/LOGIN/Login'
 import ForgotPassword from './components/LOGIN/ForgotPassword'
+import ResetPassword from './components/LOGIN/ResetPassword'
 
 function App() {
   return (
     <div className="App">
-        {/* <Home /> */}
-        {/* <Login /> */}
+        <Home /> 
+        <Login />
         <ForgotPassword />
+        <ResetPassword />
     </div>
   );
 }
---
+++ pullus-app/src/components/LOGIN/ResetPassword.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import styles from './styles.module.css'
+import Navbar from '../NAVBAR/Navbar'
+import Footer from '../FOOTER/Footer'
+
+export default function ResetPassword() {
+
+    const [data, setData] = useState({code: ''})
+
+    console.log(data)
+
+    function handleChange(event) {
+        const {name, value} = event.target
+
+        setData(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+  return (
+    <>
+        <Navbar />
+            <section className={styles.forgotPassword}>
+                <div className={styles.reset}>
+                    <h1>Reset Password</h1>
+
+                    <p>A code has been sent to your registered mobile number/email address {'+234 *** *** **54/Br********@outlook.com'}</p>
+
+                    <form>
+                        <input type={'text'} placeholder={'123-456-789'} className={styles.resetCodeInp} name={'code'} value={data.code} onChange={handleChange} />
+
+                        <button>Continue</button>
+                    </form>
+
+                </div>
+            </section>
+        <Footer />
+    </>
+  )
+}
