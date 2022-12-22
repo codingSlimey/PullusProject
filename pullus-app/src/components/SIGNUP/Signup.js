9f9008bc9ff77edf6fab95b547099c5679820c1d
codingSlimey <superslimeysl@gmail.com>
2022-12-22 21:48:04 +0200

    Created signup page boiler plate

---
+++ pullus-app/src/components/SIGNUP/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../NAVBAR/Navbar'
+import Footer from '../FOOTER/Footer'
+
+export default function Signup() {
+  return (
+    <>
+        <Navbar />
+            <section>
+
+            </section>
+        <Footer />
+    </>
+  )
+}
