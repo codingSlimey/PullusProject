98fb7325c6d6f2aaa4bc88ca1725f85168d1ff1f
codingSlimey <superslimeysl@gmail.com>
2023-01-28 14:39:11 +0200

    created biodata business info page

---
+++ pullus-app/src/components/VENDOR/BioDataBusinessInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Footer from '../FOOTER/Footer'
+import Navbar from '../NAVBAR/Navbar'
+import styles from './styles.module.css'
+
+
+export default function BioDataBusinessInfo() {
+  return (
+    <>
+        <Navbar />
+            <section className={styles.biodata}>
+                <h1>Business/Company Information</h1>
+                <form>
+                    <input type={'text'} placeholder={'Company Name'} name={''} value={''} />
+                    <input type={'text'} placeholder={'Registeration Date'} name={''} value={''} />
+                    <input type={'text'} placeholder={'RC Number'} name={''} value={''} />
+                    <input type={'text'} placeholder={'TIN Number'} name={''} value={''} />
+
+                    <input type={'button'} value={'Continue'} name={''} />
+                </form>
+            </section>
+        <Footer />
+    </>
+  )
+}
