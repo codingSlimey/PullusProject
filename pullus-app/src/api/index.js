fee2797ef81a50c7991c95b540f3bea2d755cfbc
Slenzy2 <tobiikupolati33@gmail.com>
2023-04-02 18:49:20 +0100

    fixed the issue with the endpoints

--- pullus-app/src/api/index.js
+++ pullus-app/src/api/index.js
@@ -19,7 +19,5 @@ API.interceptors.request.use((req) => {
 
 // authentication
 export const signUp = (form) => API.post('/signup/new/FirstSignupRequest', form)
-export const checkBvn = (data) => API.post('/signup/new/bvnCheck', data)
-export const getStates = () => API.post('/signup/new/statesAndLga')
-// export const checkEmail = () => API.get(`/signup/checkEmailStatus/${'tobiikupolati33@gmail.com'}`)
-// export const signUp = (form) => API.post('/auth/register', form)
+export const checkBvn = (data) => API.post('/signup/bvnCheck', data)
+export const getStates = () => API.get('/signup/statesAndLga')
