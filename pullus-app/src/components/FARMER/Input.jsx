6f145434ae79b5526c3ede121332ea286c757b3a
Margret Edeh <sagaedeh@gmail.com>
2023-04-08 13:54:12 -0700

    edited the input component

--- pullus-app/src/components/FARMER/Input.jsx
+++ pullus-app/src/components/FARMER/Input.jsx
@@ -4,6 +4,7 @@ export default function Input(props) {
 	const { name, label, extraClass } = props
 	return (
 		<>
+		<div  className='flex flex-col w-full' >
 			{label && (
 				<label
 					htmlFor={name}
@@ -17,6 +18,7 @@ export default function Input(props) {
 				{...props}
 				className={`"h-14 md:py-5 px-6 text-black/60 placeholder:text-placeholder disabled:opacity-50 disabled:cursor-not-allowed mb-6 shadow-lg border border-grey bg-[#fff]  w-full rounded-full  focus:outline-none  focus:border-none " ${extraClass}`}
 			/>
+			</div>
 		</>
 	)
 }
